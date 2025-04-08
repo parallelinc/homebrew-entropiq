54ae65429b5f0868f9ae9e19f728aa3914a47f74
Emaad Ali <3521616+emaadali@users.noreply.github.com>
2025-04-08 11:23:40 -0400

    Brew formula update for entropiq version 1.0.4

--- entropiq.rb
+++ entropiq.rb
@@ -5,14 +5,14 @@
 class Entropiq < Formula
   desc "The EntropiQ CLI."
   homepage "https://entropiq.com"
-  version "1.0.3"
+  version "1.0.4"
 
   depends_on "qemu"
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/parallelinc/entropiq-binary-cli/releases/download/1.0.3/entropiq_1.0.3_darwin_arm64.tar.gz"
-    sha256 "6d2958298efdadb83dfbd0d9ec2c79d75311a36938edd31a10fba9db5eb27a9c"
+    url "https://github.com/parallelinc/entropiq-binary-cli/releases/download/1.0.4/entropiq_1.0.4_darwin_arm64.tar.gz"
+    sha256 "172d2b2e80c8020eaf74218239f420d2d2016942843c5d9ed91946ac756bab2c"
 
     def install
       bin.install "entropiq"
