ea739006886d457d306efa7c4adb37e5317643dc
Emaad Ali <3521616+emaadali@users.noreply.github.com>
2025-04-23 18:01:29 -0400

    Brew formula update for entropiq version 1.8.13

--- entropiq.rb
+++ entropiq.rb
@@ -5,22 +5,22 @@
 class Entropiq < Formula
   desc "The EntropiQ CLI."
   homepage "https://entropiq.com"
-  version "1.8.12"
+  version "1.8.13"
 
   depends_on "qemu"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/parallelinc/entropiq-binary-cli/releases/download/1.8.12/entropiq_1.8.12_darwin_amd64.tar.gz"
-    sha256 "f4a8910be2562b1b6c28fce48790a113579fe658d5a19d62bf7768a0eae79138"
+    url "https://github.com/parallelinc/entropiq-binary-cli/releases/download/1.8.13/entropiq_1.8.13_darwin_amd64.tar.gz"
+    sha256 "c85fb4f3f7e2678cfcd9d25146cd284cfa2a33e121274067749b3af446510f5e"
 
     def install
       bin.install "entropiq"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/parallelinc/entropiq-binary-cli/releases/download/1.8.12/entropiq_1.8.12_darwin_arm64.tar.gz"
-    sha256 "f1c57ad0637f531b9eb0140b24e02881e4503e9c5249ca3b022b1d82720637ef"
+    url "https://github.com/parallelinc/entropiq-binary-cli/releases/download/1.8.13/entropiq_1.8.13_darwin_arm64.tar.gz"
+    sha256 "8e25ddcdf19ea9855fd184c7ed402e526e53fc5a27a488229396a439697573fd"
 
     def install
       bin.install "entropiq"
